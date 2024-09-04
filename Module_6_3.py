61aa20bd8dbbf2e26b1fca2f1ee92069b482a0d4
Kram-Serg <bild003@yandex.ru>
2024-09-04 14:23:58 +0700

    Update Module_6_3.py

--- Module_6_3.py
+++ Module_6_3.py
@@ -1,26 +1,31 @@
 class Horse:
 
-    x_distance = 0
     sound = 'Frrr'
 
-    def run(self, dx):
-        self.dx = dx
-        self.x_distance += dx
+    def __init__(self):
+        self.x_distance = 0
 
+    def run(self, dx):
+        self.x_distance += dx
+        
 class Eagle:
 
-    y_distance = 0
     sound = 'I train, eat, sleep, and repeat'
 
+    def __init__(self):
+        self.y_distance = 0
+
     def fly(self, dy):
-        self.dy = dy
         self.y_distance +=dy
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        super().run(dx)
-        super().fly(dy)
+        self.run(dx)
+        self.fly(dy)
 
     def get_pos(self):
         return (self.x_distance, self.y_distance)
